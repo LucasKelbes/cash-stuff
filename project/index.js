60b05cdf090490634c2b0479cfac1f5b8718be42
Mason Hedger <masonmessaging@gmail.com>
2023-04-15 13:16:44 -0500

    added chance

--- project/index.js
+++ project/index.js
@@ -1,1 +1,28 @@
+const Chance = require("chance")
+const chance = new Chance()
+
+function randomEmail(){
+    return chance.email();
+}
+
 const axios = require("axios")
+const FormData = require("form-data")
+const { File } = require("file-api")
+
+const pepURl = ""
+// const video = new File({
+//     path:".pepperoni.mp4"
+// })
+
+var formData = new FormData()
+// formData.append("name", "Whollip")
+// formData.append("email", "whollipingyourmom@gmail.com")
+// formData.append("video", )
+
+console.log(randomEmail())
+
+// axios.post(pepURL, formData, {
+//     headers: {
+//         'Content-type': "multipart/form-data"
+//     }
+// })
